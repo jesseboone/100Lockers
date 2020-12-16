a1b7c7099c29ece6150c69cea06e76bfb9e033ee
jesseboone <48326530+jesseboone@users.noreply.github.com>
2020-12-15 21:29:19 -0600

    setup psuedo for findChains

--- 100Lockers.js
+++ 100Lockers.js
@@ -1,24 +1,48 @@
 // Jesse Boone
 // 100 Lockers Problem javascript analysis
 
-
-let numLockers = 100;
+// Initializing lockers
+let numLockers = 15;
 let lockers = [];
-for (var i = 1; i <= numLockers; i++) {
+for (var i = 0; i < numLockers; i++) {
  	lockers.push(i);
 }
-	console.log(lockers);
+console.log(lockers);
 
+function findChains() {
+	let chains = [];
+	let chain = [];
+	let ndx = 0;
+	for (var i = 0; i < numLockers; i++) {
+		// if visited, skip
+		// else if (lockers[ndx] == chain[0])
+		  // complete chain (append to chains and reset to [])
+		  // CONSIDER CASE OF INDIVIDUAL CHAINS (like 9 -> 9)
+		// else 
+		  // append to chain, & set ndx to current pointer
+	}
+	return chains;
+}
 
-
-function setup() {
-	createCanvas(600, 600);
-
-	// shuffling order of lockers
+function analyze_Lockers() {
+	// Shuffling order of lockers
 	lockers = shuffle(lockers);
 	console.log(lockers);
 
+	// Find Chains
+	chains = findChains();
+
+	// Show chains
+	console.log(chains);
+
+	// Decide victory if largest chain is <= search size
 }
+
+function setup() {
+	createCanvas(600, 600);
+	analyze_Lockers();
+}
+
 
 
 
