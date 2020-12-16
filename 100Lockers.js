0d22f40136e9ffcacdb4b0d1dd9f04c8ba00d84e
jesseboone <48326530+jesseboone@users.noreply.github.com>
2020-12-15 20:44:54 -0600

    basic setup, initialize lockers and shuffle them

--- 100Lockers.js
+++ 100Lockers.js
@@ -1,12 +1,29 @@
 // Jesse Boone
 // 100 Lockers Problem javascript analysis
 
-let numLockers = 100;
 
+let numLockers = 100;
 let lockers = [];
 for (var i = 1; i <= numLockers; i++) {
-  lockers.push(i);
+ 	lockers.push(i);
 }
-lockers = shuffle(lockers);
+	console.log(lockers);
 
-console.log(lockers);
+
+
+function setup() {
+	createCanvas(600, 600);
+
+	// shuffling order of lockers
+	lockers = shuffle(lockers);
+	console.log(lockers);
+
+}
+
+
+
+
+function draw() {
+	background(255);
+
+}
